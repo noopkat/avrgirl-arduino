c67f01a2697f8ad19bc35def3927919e3f4b7fbb
noopkat <suz.hinton@gmail.com>
2015-06-23 23:21:56 -0400

    initial class setup

--- avrgirl-arduino.js
+++ avrgirl-arduino.js
@@ -1,3 +1,10 @@
-// do the thing
+var Serialport = require('serialport');
+var intelhex = require('intel-hex');
+var Stk500 = require('stk500');
+var fs = require('fs');
 
-module.exports = {};
+var Avrgirl_arduino = function (opts) {
+  this.options = {};
+};
+
+module.exports = Avrgirl_arduino;
