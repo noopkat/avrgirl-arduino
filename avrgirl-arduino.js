78f4a934164af1fc0b1b12fa39adb678057da02c
noopkat <suz.hinton@gmail.com>
2015-06-29 00:23:05 -0400

    beginnings of connect + flash without port sniffing

--- avrgirl-arduino.js
+++ avrgirl-arduino.js
@@ -2,26 +2,45 @@ var Serialport = require('serialport');
 var intelhex = require('intel-hex');
 var Stk500 = require('stk500');
 var fs = require('fs');
+var boards = require('./boards');
 
 var Avrgirl_arduino = function (opts) {
   var opts = opts || {};
 
   this.options = {
     quiet: opts.quiet || false,
-    board: opts.board || 'uno'
-  }; 
+    board: opts.board || 'uno',
+    path: opt.path || '/dev/placeholder'
+  };
+  this.board = boards[this.options.board];
+  this.stk500 = new Stk500({quiet: true});
+  this._setUpSerial();
 };
 
-Avrgirl_arduino.prototype._connect = function() {
-
+Avrgirl_arduino.prototype._setUpSerial = function() {
+  this.serialPort = new SerialPort.SerialPort(path, {
+    baudrate: this.options.board.baud,
+  }, false);
 };
 
-Avrgirl_arduino.prototype._disconnect = function() {
-
+Avrgirl_arduino.prototype._parseHex = function(file) {
+  var data = fs.readFileSync(file, {encoding: 'utf8'});
+  return intel_hex.parse(data).data;
 };
 
-Avrgirl_arduino.prototype.flash = function() {
+Avrgirl_arduino.prototype.flash = function(file) {
+  var self = this;
+  var hex = Avrgirl_arduino.prototype._parseHex(file);
 
+  serialPort.open(function (error) {
+    stk500.bootload(self.serialPort, hex, self.board, function (error) {
+      serialPort.close(function (error) {
+        console.log(error);
+      });
+      // define a 'report back' function
+      // callback(error);
+    });
+  });
 };
 
 Avrgirl_arduino.prototype.erase = function() {
