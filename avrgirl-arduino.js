1a0c7decff8f120b54e57903b4ba833bfea8578b
noopkat <suz.hinton@gmail.com>
2015-06-28 20:36:18 -0400

    placeholder methods

--- avrgirl-arduino.js
+++ avrgirl-arduino.js
@@ -4,7 +4,36 @@ var Stk500 = require('stk500');
 var fs = require('fs');
 
 var Avrgirl_arduino = function (opts) {
-  this.options = {};
+  var opts = opts || {};
+
+  this.options = {
+    quiet: opts.quiet || false,
+    board: opts.board || 'uno'
+  }; 
+};
+
+Avrgirl_arduino.prototype.connect = function(opts) {
+
+};
+
+Avrgirl_arduino.prototype.flash = function(opts) {
+
+};
+
+Avrgirl_arduino.prototype.erase = function() {
+
+};
+
+Avrgirl_arduino.prototype._flashProgram = function() {
+
+};
+
+Avrgirl_arduino.prototype._flashEEPROM = function() {
+
+};
+
+Avrgirl_arduino.prototype.read = function() {
+
 };
 
 module.exports = Avrgirl_arduino;
