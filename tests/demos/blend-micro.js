5b374fa30a371dae3291d08ee74fe2e1eaaacb95
noopkat <suz.hinton@gmail.com>
2015-07-08 21:46:13 -0400

    add blend-micro demo

---
+++ tests/demos/blend-micro.js
@@ -0,0 +1,14 @@
+var Avrgirl = require('../../avrgirl-arduino');
+var avrgirl = new Avrgirl({
+  board: 'blend-micro'
+});
+
+var hex = __dirname + '/../../junk/hex/blend-micro/Blink.cpp.hex';
+
+avrgirl.flash(hex, function (error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('done.');
+  }
+});
