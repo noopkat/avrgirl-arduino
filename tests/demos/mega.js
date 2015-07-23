d01ff6b404513446494f079b8fc8d0a6e30ffb48
Suz Hinton <suz.hinton@gmail.com>
2015-07-22 21:44:47 -0400

    add mega demo

---
+++ tests/demos/mega.js
@@ -0,0 +1,15 @@
+var Avrgirl = require('../../avrgirl-arduino');
+var avrgirl = new Avrgirl({
+  board: 'mega',
+  debug: true
+});
+
+var hex = __dirname + '/../../junk/hex/mega/Blink.cpp.hex';
+
+avrgirl.flash(hex, function (error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('done.');
+  }
+});
