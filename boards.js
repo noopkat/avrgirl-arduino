4e1a6e78822227f0ebcbe9215a1dc60c1094247f
Suz Hinton <suz.hinton@gmail.com>
2015-10-02 09:49:39 -0400

    add pro-mini to boards

--- boards.js
+++ boards.js
@@ -80,5 +80,13 @@ module.exports = {
     pollIndex:0x03,
     productId: ['0x0042'],
     protocol: 'stk500v2'
+  },
+  'pro-mini': {
+    baud: 115200,
+    signature: new Buffer([0x1e, 0x95, 0x0f]),
+    pageSize: 128,
+    numPages: 256,
+    timeout: 400,
+    protocol: 'stk500v1'
   }
 };
