ead92288602fc9e5daa8c9e8ae9a3d88259994e3
Suz Hinton <suz.hinton@gmail.com>
2015-10-03 02:57:51 -0400

    add support for Sparkfun Pro Micro

--- boards.js
+++ boards.js
@@ -88,5 +88,11 @@ module.exports = {
     numPages: 256,
     timeout: 400,
     protocol: 'stk500v1'
+  },
+  'sf-pro-micro': {
+    baud: 57600,
+    signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
+    productId: ['0x9206'],
+    protocol: 'avr109'
   }
 };
