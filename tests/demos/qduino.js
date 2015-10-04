9a313363ee8c59a114228e13a1ac78bd5d04cdad
Suz Hinton <suz.hinton@gmail.com>
2015-10-03 21:15:33 -0400

    add qduino demo file

---
+++ tests/demos/qduino.js
@@ -0,0 +1,15 @@
+var Avrgirl = require('../../avrgirl-arduino');
+var avrgirl = new Avrgirl({
+  board: 'qduino',
+  debug: true
+});
+
+var hex = __dirname + '/../../junk/hex/qduino/rainbow.cpp.hex';
+
+avrgirl.flash(hex, function (error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('done.');
+  }
+});
