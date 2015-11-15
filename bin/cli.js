9d1169707ede15b5815ced20b8f2a680ee939489
Suz Hinton <suz.hinton@gmail.com>
2015-11-15 00:35:33 -0500

    fix style on cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -3,64 +3,17 @@ var Avrgirl = require('../avrgirl-arduino');
 var boards = require('../boards');
 var parseArgs = require('minimist');
 var path = require('path');
-var util = require('util');
 
 var args = (process.argv.slice(2));
-var argv = parseArgs(args, opts={})
+var argv = parseArgs(args, {});
 var userAction = argv._[0];
-var help = "Usage:\n" +
-  "  avrgirl-arduino flash -f <file> -a <arduino name> [-p <port>] [-v]\n" +
-  "  avrgirl-arduino boards\n" +
-  "  avrgirl-arduino list";
+var help = 'Usage:\n' +
+  '  avrgirl-arduino flash -f <file> -a <arduino name> [-p <port>] [-v]\n' +
+  '  avrgirl-arduino boards\n' +
+  '  avrgirl-arduino list';
 
 function showHelp() {
   console.log(help);
-}
-
-var debug = argv.v ? console.log : function() {};
-
-handleInput(userAction, argv);
-
-function handleInput(action, argz) {
-  switch (action) {
-    case 'flash':
-      if (!argz.f || !argz.a) {
-        return showHelp();
-        process.exit(1);
-      }
-      else if (!boards.byName[argz.a]) {
-        console.error(new Error('Oops! That board is not supported, sorry.'));
-        process.exit(1);
-      }
-      else {
-        // run flash function here if all is well
-        var options = {};
-        options.board = argz.a;
-        if (argz.p) { options.port = argz.p; }
-        if (argz.v) { options.debug = true; }
-        flash(argz.f, options);
-      }
-      break;
-    case 'boards':
-      var boardNames = Object.keys( boards.byName ).sort();
-      console.log("Supported Boards:\n - " + boardNames.join("\n - ") );
-      break;
-    case 'list':
-      Avrgirl.listPorts(function(err,ports) {
-        console.log( ports );
-      });
-      break;
-    case 'help':
-      showHelp();
-      return process.exit();
-      break;
-    case undefined:
-    default:
-      // Invalid or no argument specified, show help and exit with an error status
-      showHelp();
-      return process.exit(9);
-      break;
-  }
 }
 
 function flash(file, options) {
@@ -70,10 +23,63 @@ function flash(file, options) {
   avrgirl.flash(filepath, function(error) {
     if (error) {
       console.error(error);
-      return process.exit(1);
+      process.exit(1);
     } else {
-      return process.exit();
+      process.exit();
     }
   });
-
 }
+
+function handleInput(action, argz) {
+  switch (action) {
+    case 'flash': {
+      if (!argz.f || !argz.a) {
+        showHelp();
+        process.exit(1);
+      } else if (!boards.byName[argz.a]) {
+        console.error(new Error('Oops! That board is not supported, sorry.'));
+        process.exit(1);
+      } else {
+        // run flash function here if all is well
+        var options = {
+          board: argz.a,
+          port: argz.p || '',
+          debug: argz.v || false
+        };
+
+        flash(argz.f, options);
+      }
+
+      break;
+    }
+
+    case 'boards': {
+      var boardNames = Object.keys(boards.byName).sort();
+      console.log('Supported Boards:\n - ' + boardNames.join('\n - '));
+      break;
+    }
+
+    case 'list': {
+      Avrgirl.listPorts(function(err, ports) {
+        console.log(ports);
+      });
+
+      break;
+    }
+
+    case 'help': {
+      showHelp();
+      process.exit();
+      break;
+    }
+
+    default: {
+      // Invalid or no argument specified, show help and exit with an error status
+      showHelp();
+      process.exit(9);
+      break;
+    }
+  }
+}
+
+handleInput(userAction, argv);
