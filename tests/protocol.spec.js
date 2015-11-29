87ac87ac1c2f4ff8c08a8b9cb412259bd1874e26
Suz Hinton <suz.hinton@gmail.com>
2015-11-28 19:30:52 -0500

    add protocol test

---
+++ tests/protocol.spec.js
@@ -0,0 +1,28 @@
+var test = require('tape');
+var Protocol = require('../lib/protocol.js');
+var STK = require('stk500');
+
+// default options
+var DEF_OPTS3 = {
+  protocol: STK,
+  board: {
+    baud: 115200,
+    signature: new Buffer([0x1e, 0x95, 0x0f]),
+    pageSize: 128,
+    numPages: 256,
+    timeout: 400,
+    productId: ['0x0043', '0x7523'],
+    protocol: 'stk500v1'
+  },
+  connection: {},
+  debug: false
+};
+
+test('[ Protocol ]  - new creation', function(t) {
+  t.plan(4);
+  var p = new Protocol(DEF_OPTS3);
+  t.ok(p.board, 'established board');
+  t.ok(p.connection, 'established connection');
+  t.ok(p.chip, 'established chip');
+  t.equal(typeof p._reset, 'function', 'has _reset function');
+});
