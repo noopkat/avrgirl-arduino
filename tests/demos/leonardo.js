62b7b5adcd914b5956ebc2690a3269b3d1809e72
noopkat <suz.hinton@gmail.com>
2015-12-25 20:49:58 -0700

    add leonardo demo

---
+++ tests/demos/leonardo.js
@@ -0,0 +1,15 @@
+var Avrgirl = require('../../avrgirl-arduino');
+var avrgirl = new Avrgirl({
+  board: 'leonardo',
+  debug: true
+});
+
+var hex = __dirname + '/../../junk/hex/leonardo/Blink.cpp.hex';
+
+avrgirl.flash(hex, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('done.');
+  }
+});
