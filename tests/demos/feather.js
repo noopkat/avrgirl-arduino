ce17c96af7af15f946d3358757262475712e8ef3
noopkat <suz.hinton@gmail.com>
2015-12-25 20:49:45 -0700

    add feather demo

---
+++ tests/demos/feather.js
@@ -0,0 +1,15 @@
+var Avrgirl = require('../../avrgirl-arduino');
+var avrgirl = new Avrgirl({
+  board: 'feather',
+  debug: true
+});
+
+var hex = __dirname + '/../../junk/hex/feather/Blink.cpp.hex';
+
+avrgirl.flash(hex, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('done.');
+  }
+});
