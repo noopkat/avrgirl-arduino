3a4e73a6951b277ae77d7ed57c954d6eef0361bb
Suz Hinton <suz.hinton@gmail.com>
2016-12-04 11:44:36 -0500

    add lilypad USB to supported boards

--- boards.js
+++ boards.js
@@ -171,6 +171,13 @@ var boards = [
     pollIndex:0x03,
     productId: ['0x6051'],
     protocol: 'stk500v2'
+  },
+  {
+    name: 'lilypad-usb',
+    baud: 57600,
+    signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
+    productId: ['0x9207', '0x9208', '0x1B4F'],
+    protocol: 'avr109'
   }
 ];
 
---
+++ tests/demos/lilypad-usb.js
@@ -0,0 +1,15 @@
+var Avrgirl = require('../../avrgirl-arduino');
+var avrgirl = new Avrgirl({
+  board: 'lilypad-usb',
+  debug: true
+});
+
+var hex = __dirname + '/../../junk/hex/lilypad-usb/Blink.cpp.hex';
+
+avrgirl.flash(hex, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('done.');
+  }
+});
