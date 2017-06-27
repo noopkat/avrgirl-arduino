b5aad0bebf1e724e1aafb00296a0e2782a5eb455
Gustavo A. Gratacos <gustavo.gratacos@gmail.com>
2017-06-27 11:42:16 -0500

    Changed the name to circuit-playground-classic due to new board being introduced.

--- boards.js
+++ boards.js
@@ -207,7 +207,7 @@ var boards = [
     protocol: 'avr109'
   }, 
   {
-    name: 'circuit-playground',
+    name: 'circuit-playground-classic',
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x0011', '0x8011'],
