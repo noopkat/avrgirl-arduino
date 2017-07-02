4005a9d4200d42ab0b921a49396038b15dfe548f
HipsterBrown <headhipster@hipsterbrown.com>
2017-07-02 12:51:54 -0400

    test(demo/circuit-playground-classic): adds circuit playground test demo

---
+++ tests/demos/circuit-playground-classic.js
@@ -0,0 +1,16 @@
+var Avrgirl = require('../../avrgirl-arduino');
+
+var avrgirl = new Avrgirl({
+  board: 'circuit-playground-classic',
+  debug: true
+});
+
+var hex = __dirname + '/../../junk/hex/circuit-playground-classic/Blink.cpp.hex';
+
+avrgirl.flash(hex, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('done.');
+  }
+});
