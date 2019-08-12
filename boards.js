0a5c762666672a3b1b525b1a7da5c84ff482d28a
Patrick Weingaertner <patrick.weingaertner91@gmail.com>
2019-08-12 23:35:16 +0200

    add product pages

--- boards.js
+++ boards.js
@@ -7,6 +7,7 @@ var boards = [
     numPages: 256,
     timeout: 400,
     productId: ['0x0043', '0x7523', '0x0001', '0xea60'],
+    productPage: 'https://store.arduino.cc/arduino-uno-rev3',
     protocol: 'stk500v1'
   },
   {
@@ -14,6 +15,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x0037', '0x8037', '0x0036', '0x0237'],
+    productPage: 'https://store.arduino.cc/arduino-micro',
     protocol: 'avr109'
   },
   {
@@ -21,6 +23,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x0036', '0x8037', '0x8036'],
+    productPage: 'https://www.kickstarter.com/projects/1265095814/imuduino-wireless-3d-motion-html-js-apps-arduino-p?lang=de',
     protocol: 'avr109'
   },
   {
@@ -28,6 +31,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x0036', '0x8036', '0x800c'],
+    productPage: 'https://store.arduino.cc/leonardo',
     protocol: 'avr109'
   },
   {
@@ -35,6 +39,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x0036', '0x8036', '0x800c'],
+    productPage: 'https://arduboy.com/',
     protocol: 'avr109'
   },
   {
@@ -42,6 +47,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x800c', '0x000c'],
+    productPage: 'https://www.adafruit.com/feather',
     protocol: 'avr109'
   },
   {
@@ -49,6 +55,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x0036', '0x8036'],
+    productPage: 'https://littlebits.com/collections/bits-and-accessories/products/arduino-bit',
     protocol: 'avr109'
   },
   {
@@ -56,6 +63,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x2404'],
+    productPage: 'https://redbear.cc/product/retired/blend-micro.html',
     protocol: 'avr109'
   },
   {
@@ -66,6 +74,7 @@ var boards = [
     numPages: 256,
     timeout: 400,
     productId: ['0x6001', '0x7523'],
+    productPage: 'https://web.archive.org/web/20150813095112/https://www.arduino.cc/en/Main/ArduinoBoardNano',
     protocol: 'stk500v1'
   },
   {
@@ -76,6 +85,7 @@ var boards = [
     numPages: 256,
     timeout: 400,
     productId: ['0x6001', '0x7523'],
+    productPage: 'https://store.arduino.cc/arduino-nano',
     protocol: 'stk500v1'
   },
   {
@@ -86,6 +96,7 @@ var boards = [
     numPages: 128,
     timeout: 400,
     productId: ['0x6001'],
+    productPage: 'https://www.arduino.cc/en/Main/arduinoBoardDuemilanove',
     protocol: 'stk500v1'
   },
   {
@@ -96,6 +107,7 @@ var boards = [
     numPages: 256,
     timeout: 400,
     productId: ['0x6001'],
+    productPage: 'https://www.arduino.cc/en/Main/arduinoBoardDuemilanove',
     protocol: 'stk500v1'
   },
   // the alias is here because of an accidental naming change of the tinyduino
@@ -108,6 +120,7 @@ var boards = [
     numPages: 256,
     timeout: 400,
     productId: ['0x6015'],
+    productPage: 'https://tinycircuits.com/pages/tinyduino-overview',
     protocol: 'stk500v1',
     aliases: ['tinduino']
   },
@@ -126,6 +139,7 @@ var boards = [
     pollValue:0x53,
     pollIndex:0x03,
     productId: ['0x0042', '0x6001', '0x0010', '0x7523'],
+    productPage: 'https://store.arduino.cc/mega-2560-r3',
     protocol: 'stk500v2'
   },
   {
@@ -143,6 +157,7 @@ var boards = [
     pollValue:0x53,
     pollIndex:0x03,
     productId: ['0x0044', '0x6001', '0x003F'],
+    productPage: 'https://store.arduino.cc/arduino-mega-adk-rev3',
     protocol: 'stk500v2'
   },
   {
@@ -150,6 +165,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x9206', '0x9205'],
+    productPage: 'https://www.sparkfun.com/products/12640',
     protocol: 'avr109'
   },
   {
@@ -159,6 +175,7 @@ var boards = [
     pageSize: 128,
     numPages: 256,
     timeout: 400,
+    productPage: 'https://store.arduino.cc/arduino-pro-mini',
     protocol: 'stk500v1'
   },
   {
@@ -166,6 +183,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x516d', '0x514d'],
+    productPage: 'https://www.sparkfun.com/products/13614',
     protocol: 'avr109'
   },
   {
@@ -183,6 +201,7 @@ var boards = [
     pollValue:0x53,
     pollIndex:0x03,
     productId: ['0x6051'],
+    productPage: 'https://www.mouser.de/new/crowd-supply/crowd-supply-pinoccio-microcontroller/',
     protocol: 'stk500v2'
   },
   {
@@ -190,6 +209,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x9207', '0x9208', '0x1B4F'],
+    productPage: 'https://www.sparkfun.com/products/12049',
     protocol: 'avr109'
   },
   {
@@ -197,6 +217,7 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x0041', '0x8041'],
+    productPage: 'https://store.arduino.cc/arduino-yun',
     protocol: 'avr109'
   },
   {
@@ -204,13 +225,15 @@ var boards = [
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x003C', '0x803C'],
+    productPage: 'https://store.arduino.cc/arduino-esplora',
     protocol: 'avr109'
-  }, 
+  },
   {
     name: 'circuit-playground-classic',
     baud: 57600,
     signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
     productId: ['0x0011', '0x8011'],
+    productPage: 'https://www.adafruit.com/product/3000',
     protocol: 'avr109'
   },
   /** BQ - Arduino Based Boards. Used in Bitbloq -> bitbloq.bq.com and Arduino IDE*/
@@ -222,6 +245,7 @@ var boards = [
     numPages: 256,
     timeout: 400,
     productId: ['0xEA60'],
+    productPage: 'https://store-de.bq.com/de/zum-kit-junior',
     protocol: 'stk500v1'
   },
   {
@@ -232,6 +256,7 @@ var boards = [
     numPages: 256,
     timeout: 400,
     productId: ['0xEA60'],
+    productPage: 'https://www.bq.com/de/zum-core-2-0',
     protocol: 'stk500v1'
   },
   {
@@ -242,10 +267,11 @@ var boards = [
     numPages: 256,
     timeout: 400,
     productId: ['0x6001', '0x7523'],
+    productPage: 'http://diwo.bq.com/zum-bt-328-especificaciones-tecnicas/',
     protocol: 'stk500v1'
   },
   /** END OF BQ - Arduino Based Boards. Used in Bitbloq -> bitbloq.bq.com and Arduino IDE*/
-  
+
   /** START OF Spark Concepts Boards - Arduino Based CNC Controller but uses Atmega328pb (Note 'pb' not 'p' = different signature) https://github.com/Spark-Concepts/xPro-V4 */
   {
     name: 'xprov4',
@@ -255,6 +281,7 @@ var boards = [
     numPages: 256,
     timeout: 400,
     productId: ['0x0043', '0x7523', '0x0001', '0xea60'],
+    productPage: 'http://www.spark-concepts.com/cnc-xpro-v4-controller/',
     protocol: 'stk500v1'
   },
 ];
